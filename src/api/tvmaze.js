c1732b0afd3f7e195673527eb8262d1e8b17b8c5
Harshit Sharma <harshits988@gmail.com>
2023-12-22 12:05:31 +0530

    diplay data from tvmaze website

---
+++ src/api/tvmaze.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://api.tvmaze.com'
+
+const apiGet =async (queryString) => {
+    throw new Error("Something Bad Happend");
+    //  const response = await fetch(`${BASE_URL}${queryString}`);
+    //      const body = await response.json();
+
+    //      return body;
+};
+
+
+export const searchForShows = (query) => apiGet(`/dfgtdrhgdgr?q=${query}`)
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,25 +1,56 @@
 import { useState } from 'react';
+import { searchForShows } from './../api/tvmaze'
 const Home = () => {
 
 
     const [searchStr, setSearchStr] = useState("");
+    const [apiData, setApiData] = useState(null);
+    const [apiDataError, setApiDataError] = useState(null);
+
 
     const onSearchInputChange = (ev) => {
         setSearchStr(ev.target.value);
     };
     const onSearch = async (ev) => {
         ev.preventDefault();
+        try {
+            setApiDataError(null);
 
-        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
-        const body = await response.json();
-        console.log(body);
+            const result = await searchForShows(searchStr);
+            setApiData(result);
+        } catch (error) {
+            setApiDataError(error);
+        }
     };
+
+    const renderApiData = () => {
+        if (apiDataError) {
+            return <div>Error occured: {apiDataError.message}</div>
+        }
+        if (apiData) {
+            return apiData.map((data) => (
+                <div key={data.show.id}>{data.show.name}</div>
+            ))
+
+        }
+
+        return null;
+    };
+    //{apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)}
+
+
     return (
         <div>
+
             <form onSubmit={onSearch}>
                 <input type="text" value={searchStr} onChange={onSearchInputChange} />
                 <button type="submit">Search</button>
             </form>
+
+            <div>
+                {renderApiData()}
+
+            </div>
         </div>
     );
 }
