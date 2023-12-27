cc6a797b18653514a8e5ced8933454448b76e3da
Harshit Sharma <harshits988@gmail.com>
2023-12-27 21:09:05 +0530

    installed react query replaced useShowById by react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Home from './page/Home';
 import Starred from './page/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './page/Show';
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -30,6 +34,7 @@ function App() {
   <Route path="contact-us" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,8 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 
 
 );
