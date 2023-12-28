955d30f6d4144805dfade8ebdb1700112573cb06
Harshit Sharma <harshits988@gmail.com>
2023-12-28 21:35:04 +0530

    read more button bring user to a new tab

--- src/page/Show.jsx
+++ src/page/Show.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 import { useQuery } from '@tanstack/react-query'
 import ShowMainData from '../components/shows/ShowMainData';
@@ -16,7 +16,6 @@ const Show = () => {
     });
 
 
-
     if (showError) {
         return <div>We have an error: {showError.message}</div>
     }
@@ -24,6 +23,8 @@ const Show = () => {
     if (showData) {
         return (
             <div>
+
+                <Link to="/">Go back to home</Link>
                 <ShowMainData image={showData.image} name={showData.name} rating={showData.rating} summary={showData.summary} genres={showData.genres} />
                 <div>
                     <h2>Details</h2>
