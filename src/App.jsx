f0188e9d7e37d1fe3ad9ac1ed5d4de05a8ff9841
Harshit Sharma <harshits988@gmail.com>
2023-12-31 16:15:49 +0530

    installed styled components. Create global theme with sc. created custom radio. used NavLink from react-router to style active link

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './page/Home';
 import Starred from './page/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './page/Show';
+import { GlobalTheme } from './theme';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/Starred" element={<Starred />} />
-        </Route>
-        <Route path="/show/:showId" element={<Show />} />
-        <Route path="*" element={<div>Not Found</div>} />
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/Starred" element={<Starred />} />
+            </Route>
+            <Route path="/show/:showId" element={<Show />} />
+            <Route path="*" element={<div>Not Found</div>} />
 
-        {/* <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Route>
-        <Route element={<PageLayout />}>
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/tos" element={<Tos />} />
-        </Route>
-  <Route path="contact-us" element={<Contact />} /> */}
-      </Routes>
-    </BrowserRouter>
+
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
